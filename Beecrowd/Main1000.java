25b3a5cf3865719f170fb5b7f01cff873fec79be
Sávio Cartaxo <savioferreirabeltrao1@gmail.com>
2025-06-05 22:27:17 -0300

    Primeiro dia estudando Java

---
+++ Beecrowd/Main1000.java
@@ -0,0 +1,7 @@
+package Beecrowd;
+
+public class Main1000 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Beecrowd/Main1001.java
@@ -0,0 +1,18 @@
+package Beecrowd;
+
+import java.util.*;
+
+public class Main1001{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        int z = x + y;
+        
+        sc.close();
+        
+        System.out.println("X = " + z);
+    }
+}
